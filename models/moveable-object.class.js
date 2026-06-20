5dc3ccc17ddabe502c7fd61f0e7274a4e165eca4
EnricoHof <enrico.junior@icloud.com>
2026-06-20 11:37:36 +0200

    Initiales Setup: OOP-Grundgeruest mit Canvas-Rendering
    - MovableObject als Superklasse, Character und Chicken erben davon
    - World zeichnet Charakter und Gegner per requestAnimationFrame
    - Canvas 720x480, Asset-Ordner eingebunden

---
+++ models/moveable-object.class.js
@@ -0,0 +1,19 @@
+class moveableObject {
+  x = 120;
+  y = 200;
+  img;
+  height = 150;
+  width = 100;
+
+  loadImage(path) {
+    this.img = new Image();
+    this.img.src = path;
+  }
+
+  moveRight() {
+    console.log("Move right");
+  }
+  moveLeft() {
+    console.log("Move left");
+  }
+}
